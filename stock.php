a0be360313d1c3094de06add59001e3133ce312c
RafaelBruno99 <55978113+RafaelBruno99@users.noreply.github.com>
2021-04-27 23:21:40 +0100

    Stock table, Orders Table - Admin Panel Set up.

--- stock.php
+++ stock.php
@@ -28,6 +28,48 @@
 
 <div class="w3-content" style="max-width:1100px" id="orders">
 
+<div class="w3-row w3-padding-64" id="about">
+        <button class="button-design2" id="myBtn">Add Stock</button>
+</div>
+
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
   <table class="styled-table">
       <thead>
           <tr>
